746df32891cbab4e77e688a689d36fb6c523ddf1
karan7887 <karanchoudhary7887@gmail.com>
2020-06-22 21:23:11 +0530

    stopwords-update

--- app.py
+++ app.py
@@ -11,12 +11,11 @@ cv = joblib.load('vector2.pkl')
 import nltk
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem import PorterStemmer
-from nltk.corpus import stopwords
 
 ps = PorterStemmer()
 stemmer = PorterStemmer()
 tokenizer = RegexpTokenizer('[a-z0-9]+')
-sw = set(stopwords.words('english'))
+sw = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
 def transfor(message):
     message = message.lower()
